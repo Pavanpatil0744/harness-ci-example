052fe61a3d02d4ef8a4296569002bcd812d4b204
Pavanpatil0744 <109224601+Pavanpatil0744@users.noreply.github.com>
2023-12-07 16:58:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (_req, res) =>  {
-  res.status(200).send("Hello World!")
+  res.status(200).send("Hello harness!")
 })
 module.exports = app;
